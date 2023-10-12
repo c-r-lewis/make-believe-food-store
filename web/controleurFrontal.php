ea05cb7133a511a901f5ee96472dc17e4c5d2234
Charlotte <lewisc@K141-23.dep-info.iutmontp.univ-montp2.fr>
2023-10-12 12:03:51 +0200

    Permettre au controleur frontal d'utiliser l'autoloader

--- web/controleurFrontal.php
+++ web/controleurFrontal.php
@@ -1,1 +1,10 @@
 <?php
+
+require_once __DIR__ . '/../src/Lib/Psr4AutoloaderClass.php';
+// initialisation
+$loader = new App\Covoiturage\Lib\Psr4AutoloaderClass();
+$loader->register();
+// enregistrement d'une association "espace de nom" → "dossier"
+$loader->addNamespace('App\Covoiturage', __DIR__ . '/../src');
+
+
