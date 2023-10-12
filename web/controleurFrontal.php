535109ade355256617dfa3768a2c057f08205419
Charlotte <lewisc@K141-23.dep-info.iutmontp.univ-montp2.fr>
2023-10-12 12:05:24 +0200

    AJouter une condition permettant de vérifier que le controleur a été donné

--- web/controleurFrontal.php
+++ web/controleurFrontal.php
@@ -7,4 +7,29 @@ $loader->register();
 // enregistrement d'une association "espace de nom" → "dossier"
 $loader->addNamespace('App\Covoiturage', __DIR__ . '/../src');
 
+if (isset($_GET['controleur'])) {
+    $controleur = $_GET['controleur'];
+} else {
+    $controleur = "voiture";
+}
+
+$nomDeClasseControleur = '\App\Covoiturage\Controleur\Controleur' . ucfirst($controleur);
+
+if (class_exists($nomDeClasseControleur)) {
+    // On récupère l'action passée dans l'URL
+    if (isset($_GET['action'])) {
+        $action = $_GET['action'];
+    } else {
+        $action = "afficherListe";
+    }
+    if (!in_array($action, get_class_methods($nomDeClasseControleur))) {
+        $nomDeClasseControleur::afficherErreur(" L'action n'existe pas dans " . $nomDeClasseControleur);
+    } else {
+        // Appel de la méthode statique $action du contrôleur
+        $nomDeClasseControleur::$action();
+    }
+}
+else {
+    ControleurVoiture::afficherErreur(" Le controleur n'existe pas !");
+}
 
