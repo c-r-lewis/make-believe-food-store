dffacb267591ca5f596f08e7055b24d2313373e5
Lewis <charlotte.rose.lewis.04@gmail.com>
2023-10-13 20:28:35 +0200

    Création vue basique (à rendre dynamique) pour modifier un produit

--- src/vues/admin/formulaireMettreAJourProduit.php
+++ src/vues/admin/formulaireMettreAJourProduit.php
@@ -1,1 +1,31 @@
-<?php
+<div>
+    <form enctype="multipart/form-data">
+        <fieldset>
+            <legend>Présentation générale</legend>
+            <p>
+                <label>Titre</label>
+                <input type="text"/>
+            </p>
+            <p>
+                <label>Description</label>
+                <input type="text"/>
+            </p>
+        </fieldset>
+        <fieldset>
+            <legend>Prix</legend>
+            <p>
+                <label>Prix total</label>
+                <input type="text"/>
+            </p>
+            <p>
+                <label>Prix par objet</label>
+                <input type="text"/>
+            </p>
+        </fieldset>
+        <fieldset>
+            <label>Images</label>
+            <input type="file" accept=".jpg, .jpeg, .png">
+        </fieldset>
+
+    </form>
+</div>
