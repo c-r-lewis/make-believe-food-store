ce2a589165a3c1a8682f2749915ec2b4f8533d70
Lewis <charlotte.rose.lewis.04@gmail.com>
2023-10-13 19:09:19 +0200

    Formulaire générique du côté administrateur pour créer un produit

---
+++ src/vues/admin/formulaireCreerProduit.php
@@ -0,0 +1,30 @@
+<div>
+    <form>
+        <fieldset>
+            <legend>Présentation générale</legend>
+            <p>
+                <label>Titre</label>
+                <input type="text"/>
+            </p>
+            <p>
+                <label>Description</label>
+                <input type="text"/>
+            </p>
+        </fieldset>
+        <fieldset>
+            <legend>Prix</legend>
+            <p>
+                <label>Prix total</label>
+                <input type="text"/>
+            </p>
+            <p>
+                <label>Prix par objet</label>
+                <input type="text"/>
+            </p>
+        </fieldset>
+        <fieldset>
+            <label>Images</label>
+        </fieldset>
+
+    </form>
+</div>
--- web/controleurFrontal.php
+++ web/controleurFrontal.php
@@ -1,1 +1,4 @@
 <?php
+
+require_once __DIR__ . '/../src/Lib/Psr4AutoloaderClass.php';
+
