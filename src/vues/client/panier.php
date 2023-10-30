39ffdf5061a1fa0b96b7f32d778af0eb10a23f28
Lewis <charlotte.rose.lewis.04@gmail.com>
2023-10-30 09:54:08 +0100

    Créer une partie dans le panier pour récapituler et valider l'achat

--- src/vues/client/panier.php
+++ src/vues/client/panier.php
@@ -1,17 +1,35 @@
 <section>
-    <div class="line"></div>
+
     <div class="panier">
         <div class="items">
-            <img src="" alt="Produit">
-            <p>Nom produit</p>
-            <!-- Il faudra récupérer le prix unitaire stocké dans la base de données -->
-            <input type="number" min="1" value="1" id="quantite" data-price="10" oninput="mettreAJourPrixTotal(this)">
-            <p><span id="prixTotal"></span></p>
+            <div class="block">
+                <p>Panier</p>
 
-            <button class="panier-supprimer">
-                <img src="../../../ressources/images/logo-fermer.png"/>
-            </button>
+                <div class="line"></div>
+
+                <div class="item">
+                    <img src="" alt="Produit">
+                    <p>Nom produit</p>
+                    <!-- Il faudra récupérer le prix unitaire stocké dans la base de données -->
+                    <input type="number" min="1" value="1" id="quantite" data-price="10" oninput="mettreAJourPrixTotal(this)">
+                    <p><span id="prixTotal"></span></p>
+
+                    <button class="panier-supprimer">
+                        <img src="../../../ressources/images/logo-fermer.png"/>
+                    </button>
+                </div>
+
+                <div class="line"></div>
+            </div>
+        </div>
+        <div class="sommaire">
+            <div class="block">
+                <p>Nombre d'items</p>
+                <p>Expédition</p>
+                <button>Valider</button>
+            </div>
         </div>
     </div>
+
 </section>
 
