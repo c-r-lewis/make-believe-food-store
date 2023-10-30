885a555259d844e33860f0863f6ecc1b66867a15
Lewis <charlotte.rose.lewis.04@gmail.com>
2023-10-30 10:34:16 +0100

    Création d'une première interface pour permettre à l'amdministrateur de regarder/modifier/supprimer des comptes

--- src/vues/admin/comptes.php
+++ src/vues/admin/comptes.php
@@ -4,6 +4,7 @@
     <div class="item">
         <img src="../../../ressources/images/logo-utilisateur.png">
         <p>Login utilisateur</p>
+        <p>Role</p>
         <button><img src="../../../ressources/images/logo-modifier.png"></button>
         <button><img src="../../../ressources/images/logo-supprimer.png"></button>
     </div>
