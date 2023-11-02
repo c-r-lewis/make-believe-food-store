6145a639692ccc41ef7439c106a729dd766c1272
Lewis <charlotte.rose.lewis.04@gmail.com>
2023-11-02 16:10:12 +0100

    Permettre d'enregistrer le panier dans un cookie quand l'utilisateur n'est pas connecté

--- src/Modeles/DataObject/Panier.php
+++ src/Modeles/DataObject/Panier.php
@@ -1,6 +1,29 @@
 <?php
 
 namespace App\Magasin\Modeles\DataObject;
+use App\Magasin\Lib\ConnexionUtilisateur as ConnexionUtilisateur;
+use App\Magasin\Modeles\HTTP\Cookie as Cookie;
 class Panier {
 
+    public static function ajouterItem(int $idProduit, int $quantite) : void {
+        if (!ConnexionUtilisateur::estConnecte()) {
+            self::enregistrerPanierEnTantQueCookie($idProduit, $quantite);
+        }
+    }
+
+    private static function enregistrerPanierEnTantQueCookie(int $idProduit, int $quantite) : void {
+        if (!Cookie::contient("panier")){
+            $panier[$idProduit] = $quantite;
+        }
+        else {
+            $panier = Cookie::lire("panier");
+            if(array_key_exists($idProduit, $panier)) {
+                $panier[$idProduit] += $quantite;
+            }
+            else {
+                $panier[$idProduit] = $quantite;
+            }
+        }
+        Cookie::enregistrer("panier", $panier);
+    }
 }
