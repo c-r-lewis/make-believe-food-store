fa6339e86e1b5a8945f2dc84d665d0b60bc4b2a5
Paul Jacquemin <paul.jacquemin@umontpellier.fr>
2023-11-14 18:28:01 +0100

    Ajouté une vérif du mot de passe de confirmation

--- src/Controleurs/ControleurUtilisateurGenerique.php
+++ src/Controleurs/ControleurUtilisateurGenerique.php
@@ -8,80 +8,87 @@ use App\Magasin\Modeles\HTTP\Cookie;
 use App\Magasin\Modeles\Repository\ProduitRepository;
 use App\Magasin\Modeles\Repository\UtilisateurRepository;
 
-class ControleurUtilisateurGenerique extends ControleurGenerique {
+class ControleurUtilisateurGenerique extends ControleurGenerique
+{
 
-    public static function afficherPanier() : void {
+    public static function afficherPanier(): void
+    {
         $produits = [];
         if (ConnexionUtilisateur::estConnecte()) {
             //TODO: récupérer les produits à partir de la base de données
-        }
-        else {
+        } else {
             if (Cookie::contient("panier")) {
                 $panier = Cookie::lire("panier");
 
                 foreach ($panier as $idProduit => $quantite) {
-                    $produits[] = ["produit"=>(new ProduitRepository())->recupererParClePrimaire($idProduit),
-                        "quantite"=>$quantite];
+                    $produits[] = ["produit" => (new ProduitRepository())->recupererParClePrimaire($idProduit),
+                        "quantite" => $quantite];
                 }
             }
         }
-        self::afficherVue("vueGenerale.php", ["cheminVueBody"=>"utilisateur/client/panier.php", "produits"=>$produits]);
+        self::afficherVue("vueGenerale.php", ["cheminVueBody" => "utilisateur/client/panier.php", "produits" => $produits]);
     }
 
-    public static function afficherHistorique(): void {
-
-    }
-
-
-    public static function afficherConnexion(): void {
-        self::afficherVue("vueGenerale.php", ["cheminVueBody"=>"utilisateur/connexion.php"]);
-    }
-
-    public static function afficherParametres() : void {
-        self::afficherVue("vueGenerale.php", ["cheminVueBody"=>"utilisateur/parametres.php"]);
+    public static function afficherHistorique(): void
+    {
 
     }
 
-    public static function afficherInscription() : void {
-        self::afficherVue("vueGenerale.php", ["cheminVueBody"=>"utilisateur/inscription.php"]);
+
+    public static function afficherConnexion(): void
+    {
+        self::afficherVue("vueGenerale.php", ["cheminVueBody" => "utilisateur/connexion.php"]);
     }
 
-    public static function afficherComptes(): void {
-        self::afficherVue("vueGenerale.php", ["cheminVueBody"=>"utilisateur/admin/comptes.php"]);
+    public static function afficherParametres(): void
+    {
+        self::afficherVue("vueGenerale.php", ["cheminVueBody" => "utilisateur/parametres.php"]);
+
+    }
+
+    public static function afficherInscription(): void
+    {
+        self::afficherVue("vueGenerale.php", ["cheminVueBody" => "utilisateur/inscription.php"]);
+    }
+
+    public static function afficherComptes(): void
+    {
+        self::afficherVue("vueGenerale.php", ["cheminVueBody" => "utilisateur/admin/comptes.php"]);
     }
 
 
-    public static function  inscription() : void {
-        if ($_SERVER["REQUEST_METHOD"]=="GET") {
-            if (!(new UtilisateurRepository())->clePrimaireExiste($_GET["email"])) {
+    public static function inscription(): void
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            if ((new UtilisateurRepository())->clePrimaireExiste($_GET["email"])) {
+                // le compte est déjà enregistré
+            } elseif ($_GET["mdp"] != $_GET["mdp2"]) {
+                // les mots de passe ne correspondent pas
+            } else {
                 $utilisateur = new Utilisateur($_GET["email"], $_GET["nom"], $_GET["prenom"], $_GET["mdp"]);
                 (new UtilisateurRepository())->sauvegarder($utilisateur);
                 self::connexion();
             }
-            else {
-                //Gérer l'erreur
-            }
-
         }
     }
 
-    public static function connexion() : void {
-        if ($_SERVER["REQUEST_METHOD"]=="GET") {
-            if ((new UtilisateurRepository())->clePrimaireExiste($_GET["email"])){
+    public static function connexion(): void
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            if ((new UtilisateurRepository())->clePrimaireExiste($_GET["email"])) {
                 ConnexionUtilisateur::connecter($_GET["email"]);
                 ControleurProduit::afficherCatalogue();
-            }
-            else {
+            } else {
                 // Gérer l'erreur
             }
         }
     }
 
-    public static function deconnexion() : void {
+    public static function deconnexion(): void
+    {
         ConnexionUtilisateur::deconnecter();
         ControleurProduit::afficherCatalogue();
     }
-
 
 
 }
