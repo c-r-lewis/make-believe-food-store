0aabf4d954f1709961d689dbe4b7add712f812c2
Paul Jacquemin <paul.jacquemin@umontpellier.fr>
2023-11-14 18:04:29 +0100

    Ajouté un poivre

--- src/Lib/MotDePasse.php
+++ src/Lib/MotDePasse.php
@@ -6,18 +6,18 @@ class MotDePasse
 {
 
 // Exécutez genererChaineAleatoire() et stockez sa sortie dans le poivre
-private static string $poivre = "";
+    private static string $poivre = "QnD2cbN4jipTkreM05KDNK";
 
-public static function hacher(string $mdpClair): string
-{
-$mdpPoivre = hash_hmac("sha256", $mdpClair, MotDePasse::$poivre);
-$mdpHache = password_hash($mdpPoivre, PASSWORD_DEFAULT);
-return $mdpHache;
-}
+    public static function hacher(string $mdpClair): string
+    {
+        $mdpPoivre = hash_hmac("sha256", $mdpClair, MotDePasse::$poivre);
+        $mdpHache = password_hash($mdpPoivre, PASSWORD_DEFAULT);
+        return $mdpHache;
+    }
 
-public static function verifier(string $mdpClair, string $mdpHache): bool
-{
-$mdpPoivre = hash_hmac("sha256", $mdpClair, MotDePasse::$poivre);
-return password_verify($mdpPoivre, $mdpHache);
-}
+    public static function verifier(string $mdpClair, string $mdpHache): bool
+    {
+        $mdpPoivre = hash_hmac("sha256", $mdpClair, MotDePasse::$poivre);
+        return password_verify($mdpPoivre, $mdpHache);
+    }
 }
