1362e7ffcb9b240914c40c63918e5dbe479e3bcd
Paul Jacquemin <paul.jacquemin@umontpellier.fr>
2023-11-18 19:09:13 +0100

    Ajout de l'appel à l'action et dans le formulaire parametres.php

--- src/vues/utilisateur/parametres.php
+++ src/vues/utilisateur/parametres.php
@@ -18,29 +18,34 @@ use \App\Magasin\Modeles\Repository\UtilisateurRepository as UtilisateurReposito
             ?>
 
         </div>
-        <p>Informations personelles</p>
-        <?php
-        $utilisateur = (new UtilisateurRepository)->recupererParClePrimaire($login)
-        ?>
-        <div class="block-connexion">
-            <div style="margin-right: 15px">
-                <label for="nom">Nom</label>
-                <input type="text" id="nom" name="nom" value="<?= $utilisateur->getNom() ?>">
+        <form method="get" action="../web/controleurFrontal.php">
+            <p>Informations personelles</p>
+            <?php
+            $utilisateur = (new UtilisateurRepository)->recupererParClePrimaire($login)
+            ?>
+            <div class="block-connexion">
+                <div style="margin-right: 15px">
+                    <label for="nom">Nom</label>
+                    <input type="text" id="nom" name="nom" value="<?= $utilisateur->getNom() ?>">
+                </div>
+                <div>
+                    <label for="prenom">Prenom</label>
+                    <input type="text" id="prenom" name="prenom" value="<?= $utilisateur->getPrenom() ?>">
+                </div>
             </div>
-            <div>
-                <label for="prenom">Prenom</label>
-                <input type="text" id="prenom" name="prenom" value="<?= $utilisateur->getPrenom() ?>">
-            </div>
-        </div>
-        <label for="email">Email</label>
-        <input type="text" id="email" name="email" value="<?= $utilisateur->getEmail() ?>">
-        <h4>Mot de passe</h4>
-        <label for="mpdActuel">Mot de passe actuel</label>
-        <input type="text" id="mpdActuel" name="mdpActuel">
-        <label for="mdpNouveau">Nouveau mot de passe</label>
-        <input type="text" id="mdpNouveau" name="mdpNouveau">
-        <label for="mdpNouveau2">Confirmation mot de passe</label>
-        <input type="text" id="mdpNouveau2" name="mdpNouveau2">
-        <input class="button" type="submit" value="Sauvegarder">
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" value="<?= $utilisateur->getEmail() ?>">
+            <h4>Mot de passe</h4>
+            <label for="mpdActuel">Mot de passe actuel</label>
+            <input type="text" id="mpdActuel" name="mdpActuel">
+            <label for="mdpNouveau">Nouveau mot de passe</label>
+            <input type="text" id="mdpNouveau" name="mdpNouveau">
+            <label for="mdpNouveau2">Confirmation mot de passe</label>
+            <input type="text" id="mdpNouveau2" name="mdpNouveau2">
+            <input type="hidden" name="action" value="miseAJourParametres">
+            <input type="hidden" name="controleur" value="utilisateurGenerique">
+            <input class="button" type="submit" value="Sauvegarder">
+
+        </form>
     </div>
 </section>
