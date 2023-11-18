bcd4e4c8ada12bd897a142ad68d7ddcc2f595f93
Jade <jade.renaut@gmail.com>
2023-11-18 20:21:45 +0100

    fonction message flash quand les mdp sont pas pareil

---
+++ ressources/scripts/fonctionsInscription.js
@@ -0,0 +1,20 @@
+function checkPasswordMatch() {
+    var password = document.getElementById("mdp").value;
+    var confirmPassword = document.getElementById("mdp2").value;
+
+    if (password !== confirmPassword) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validateForm(event) {
+    if (!checkPasswordMatch()) {
+        var url = '../web/controleurFrontal.php?action=afficherInscription&controleur=utilisateurGenerique&messagesFlash[warning][]=MDP+Differents';
+        window.location.href = url;
+        event.preventDefault();
+        return false;
+    }
+    return true;
+}
--- src/vues/utilisateur/inscription.php
+++ src/vues/utilisateur/inscription.php
@@ -1,38 +1,56 @@
-<section class="block-connexion">
-    <div class="bienvenue">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title</title>
+</head>
+<body>
+
+<section class='block-connexion'>
+    <div class='bienvenue'>
         <p>BIENVENUE</p>
     </div>
-    <div class="infos-compte">
-        <form method="get" action="../web/controleurFrontal.php">
+    <div class='infos-compte'>
+        <form method='get' action='../web/controleurFrontal.php' onsubmit="return validateForm(event)">
             <p>S'inscrire</p>
             <p>
-                <input type="text" placeholder="Email" name="email">
+                <input type='text' placeholder='Email' name='email' required>
+                <?php
+                // PHP code for displaying error message
+                use App\Magasin\Lib\MessageFlash;
+                if (isset($loginDejaPris) && $loginDejaPris) {
+                    echo "<p class='error-message'>L'adresse email est déjà utilisée</p>";
+                }
+                ?>
             </p>
-            <div class="info-details">
+            <div class='info-details'>
                 <p>
-                    <input type="text" placeholder="Nom" name="nom">
+                    <input type='text' placeholder='Nom' name='nom' required>
                 </p>
                 <p>
-                    <input type="text" placeholder="Prenom" name="prenom">
+                    <input type='text' placeholder='Prenom' name='prenom' required>
                 </p>
             </div>
-            <div class="info-details">
-                <p class="InputAddOn">
-                    <input class="InputAddOn-field" type="password" placeholder="Mot de passe" name="mdp" required>
+            <div class='info-details'>
+                <p class='InputAddOn'>
+                    <input class='InputAddOn-field' type='password' placeholder='Mot de passe' name='mdp' id='mdp' required>
                 </p>
-                <p class="InputAddOn">
-                    <input class="InputAddOn-field" type="password" placeholder="Confirmer mot de passe" name="mdp2" required>
+                <p class='InputAddOn'>
+                    <input class='InputAddOn-field' type='password' placeholder='Confirmer mot de passe' name='mdp2' id='mdp2' oninput="checkPasswordMatch()" required>
                 </p>
-
+                <p id="password-match-message" class="error-message"></p>
             </div>
             <p>
-                <input class="button" type="submit" value="S'inscrire">
-                <input type="hidden" name="action" value="inscription">
-                <input type="hidden" name="controleur" value="utilisateurGenerique">
+                <input class='button' type='submit' value="S'inscrire">
+                <input type='hidden' name='action' value='inscription'>
+                <input type='hidden' name='controleur' value='utilisateurGenerique'>
             </p>
-            <p><span id="detail">Déjà un compte ? <a href="controleurFrontal.php?action=afficherConnexion&controleur=utilisateurGenerique">Connectez-vous</a></p>
-
-
+            <p><span id='detail'>Déjà un compte ? <a href='controleurFrontal.php?action=afficherConnexion&controleur=utilisateurGenerique'>Connectez-vous</a></span></p>
         </form>
     </div>
-</section>
+
+    <script src="../../ressources/scripts/fonctionsInscription.js"></script>
+
+</body>
+</html>
