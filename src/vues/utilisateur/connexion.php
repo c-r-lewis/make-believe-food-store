a6bb8ba39e98df9b7d5c3d153bcf7a96c2b4a21a
Charlotte <charlotte.lewis@etu.umontpellier.fr>
2023-11-30 12:29:06 +0100

    refactor : login.php

--- src/vues/utilisateur/connexion.php
+++ src/vues/utilisateur/connexion.php
@@ -1,26 +1,26 @@
-<section class="block-connexion">
-    <div class="infos-compte">
 
-        <form method="get" action="../web/controleurFrontal.php">
-            <p>Se connecter</p>
-            <p>
-                <input class="input" name="email" type="text" placeholder="Email" required/>
-            </p>
+<main class="d-flex align-items-center">
+    <div class="container-fluid p-3">
+        <div class="row gx-0" style="height: 80dvh">
+            <div class="col-6 d-flex align-items-center flex-column
+            justify-content-center data-entry rounded-start">
+                <span class="mb-5">Se connecter</span>
+                <form method="post" action="" class="d-flex align-items-center flex-column justify-content-center">
+                    <input class="mb-4" name="email" type="text" placeholder="Email" required/>
+                    <input class="mb-4" name="mdp" type="text" placeholder="Mot de passe" required/>
 
-            <p>
-                <input class="input" name="mdp" type="text" placeholder="Mot de passe" required/>
-            </p>
-            <p>
-                <input class="button" type="submit" value="Se connecter">
-                <input type="hidden" name="action" value="connexion">
-                <input type="hidden" name="controleur" value="utilisateurGenerique">
-            </p>
-            <p><span id="detail">Pas de compte ? <a href="controleurFrontal.php?action=afficherInscription&controleur=utilisateurGenerique">Inscrivez-vous</a></p>
-        </form>
+                    <input class="texte-white mb-3" type="submit" value="Se connecter">
+                    <input type="hidden" name="action" value="connexion">
+                    <input type="hidden" name="controleur" value="utilisateurGenerique">
+                </form>
+                <span>Pas de compte ? <a href="controleurFrontal.php?action=afficherInscription&controleur=utilisateurGenerique">Inscrivez-vous</a></span>
+            </div>
+            <div class="col-6 d-flex align-items-center flex-column
+                 justify-content-center filler rounded-end">
+                <span class="text-white">Bienvenue</span>
+            </div>
+        </div>
     </div>
-    <div class="bienvenue">
-        <p>BIENVENUE</p>
-    </div>
-</section>
+</main>
 
 
