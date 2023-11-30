bfe3c5fb1af0e2096595b0611e7e4e1796a29d0c
Jade <jade.renaut@etu.umontpellier.fr>
2023-11-30 21:42:30 +0100

    correction suppression de compte

--- src/vues/utilisateur/admin/comptes.php
+++ src/vues/utilisateur/admin/comptes.php
@@ -8,7 +8,7 @@
         <div class="item">
             <img src="../../../../ressources/images/logo-utilisateur.png">
             <p><?php echo $compte->toString(); ?></p>
-            <a href="controleurFrontal.php?action=supprimerCompte&email=<?php echo urlencode($compte->getEmail()); ?>">
+            <a href="controleurFrontal.php?action=supprimerCompte&controleur=UtilisateurGenerique&email=<?php echo urlencode($compte->getEmail()); ?>">
                 <button><img src="../../../../ressources/images/logo-supprimer.png"></button>
             </a>
         </div>
