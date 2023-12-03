088a14c076ffb6bbbf715dcb5f8f73eba2abe593
Paul Jacquemin <paul.jacquemin@umontpellier.fr>
2023-12-03 14:36:54 +0100

    feat: fait en sorte que la vue catalogue aille chercher le chemin des images des produits dans la base de données

--- src/vues/produit/catalogue.php
+++ src/vues/produit/catalogue.php
@@ -3,9 +3,15 @@
         <div class="row">
             <?php
             use App\Magasin\Lib\ConnexionUtilisateur as ConnexionUtilisateur;
+            use App\Magasin\Modeles\DataObject\Image;
+            use App\Magasin\Modeles\DataObject\Produit;
+            use App\Magasin\Modeles\Repository\ImageRepository;
             use App\Magasin\Modeles\Repository\UtilisateurRepository as UtilisateurRepository;
 
             /** @var array $produits */
+            /** @var Image $image */
+            /** @var Produit $produit */
+
 
             $action = "afficherDetailProduit";
             if (ConnexionUtilisateur::estConnecte()) {
@@ -18,9 +24,15 @@
             }
 
             foreach ($produits as $produit) {
+                $repo = new ImageRepository();
+                $cheminImage = "../ressources/images/placeholder.png";
+                if (count($repo->recupererParClePrimaire([$produit->getIdProduit()])) != 0) {
+                    $image = $repo->recupererParClePrimaire([$produit->getIdProduit()])[0];
+                    $cheminImage = $image->getImage();
+                }
                 echo '<div class="col-md-3 mb-3">
             <div class="card shadow text-center">
-                <img src="https://picsum.photos/200" class="card-img-top" alt="...">
+                <img src="' . $cheminImage . '" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title fs-6">'.htmlspecialchars($produit->getNomProduit()).'</h5>
                     <a href="controleurFrontal.php?action='.$action.'&idProduit='.$produit->getIdProduit().'" class="btn btn-outline-secondary">Voir produit</a>
